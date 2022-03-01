24f7a753a6d481831e6ea29b2aa819526b82f71b
Doğukan Göker <dogukangkr1234@gmail.com>
2022-03-02 00:45:07 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import { saveAs } from 'file-saver';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState('');
+  const [fileName, setFileName] = useState('');
+  const [error, setError] = useState('');
+
+  const generatePassword = (passwordLength, text) => {
+    setError('');
+    setResult('');
+    const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!=+-*/%&@#$?";
+    const result = Array(passwordLength).fill(0).map(() => characters[Math.floor(Math.random() * characters.length)]).join('');
+    if(!text) return setError('Please enter text to generate password');
+    setResult(result);
+    const blob = new Blob([result + ' ' + text + ' password'], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, `${text} password.txt`);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Password Generator</h2>
+        <label htmlFor='password'>Enter text to generate password</label>
+        <input  type="text" id="password" onChange={(e) => setFileName(e.target.value)} />
+        <button onClick={() => generatePassword(12, fileName)}>Generate</button>
+        <h5>{result}</h5>
+        <h5>{error}</h5>
+      </header>      
     </div>
   );
 }
